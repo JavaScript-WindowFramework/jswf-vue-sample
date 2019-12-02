e708c302436d68d4db2b8d4a4069285fbef3397a
SoraKumo <info@croud.jp>
2019-12-02 18:30:57 +0900

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- Add JSWindow-->
+    <JSWindow title="Virtual Window">
+      <HelloWorld msg="Welcome to Your Vue.js App" />
+    </JSWindow>
+    <!---------------->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import { JSWindow } from "@jswf/vue"; //Add
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    JSWindow //Add
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
