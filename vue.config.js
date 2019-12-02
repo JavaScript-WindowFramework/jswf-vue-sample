fb1363256d9fd403355af391ea50d680659e9efa
SoraKumo <info@croud.jp>
2019-12-02 21:49:27 +0900

    デプロイ用の階層調整

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
-module.exports = {
+const config = {
   configureWebpack: {
     resolve: { symlinks: false }
   }
 };
+const rep = process.env.GITHUB_REPOSITORY;
+const routerBase = rep
+  ? {
+      baseUrl: rep.substring(rep.lastIndexOf("/")) + "/"
+    }
+  : {};
+module.exports = { ...config, ...routerBase };
